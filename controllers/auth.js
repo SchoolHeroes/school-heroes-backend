308bbab877835a785e305bc6f8604ba6e8e4e6f1
RoMan-robots <romkozhan@gmail.com>
2024-11-22 20:10:28 +0200

    from route to validateLoginData controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,31 @@
-const loginWithEmailAndPassword = ({ email, password}) => {
-    if(email && password) {
+const validateLoginData = ({ email, phone, password, googleToken, appleToken, res }) => {
+    const loginMethods = {
+        "Email and Password": ctrl.loginWithEmailAndPassword,
+        "Phone and Password": ctrl.loginWithPhoneAndPassword,
+        "Google Login": ctrl.loginWithGoogle,
+        "Apple Login": ctrl.loginWithApple,
+    };
+
+    for (const controller of Object.entries(loginMethods)) {
+        const result = controller({ email, phone, password, googleToken, appleToken });
+        if (result) {
+            res.status(200).json("Success login");
+            return
+        }
+    }
+    res.status(400).json({ message: "Invalid login credentials" });
+};
+
+const loginWithEmailAndPassword = ({ email, password }) => {
+    if (email && password) {
         // Логіка авторизації емейлу і пароля
     } else {
         return
     }
 }
 
-const loginWithPhoneAndPassword = ({ phone, password}) => {
-    if(phone && password) {
+const loginWithPhoneAndPassword = ({ phone, password }) => {
+    if (phone && password) {
         // Логіка авторизації телефона і пароля
     } else {
         return
@@ -15,7 +33,7 @@ const loginWithPhoneAndPassword = ({ phone, password}) => {
 }
 
 const loginWithGoogle = ({ googleToken }) => {
-    if(googleToken) {
+    if (googleToken) {
         // Логіка авторизації з гугл акаунта
     } else {
         return
@@ -23,9 +41,17 @@ const loginWithGoogle = ({ googleToken }) => {
 }
 
 const loginWithApple = ({ appleToken }) => {
-    if(appleToken) {
+    if (appleToken) {
         // Логіка авторизації з епл акаунта
     } else {
         return
     }
 }
+
+module.exports = {
+    validateLoginData,
+    loginWithEmailAndPassword,
+    loginWithPhoneAndPassword,
+    loginWithGoogle,
+    loginWithApple,
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,28 +5,7 @@ const router = express.Router();
 
 router.post('/login', (req, res) => {
     const { email, phone, password, googleToken, appleToken } = req.body;
-    const loginMethods = {
-        "Email and Password": ctrl.loginWithEmailAndPassword,
-        "Phone and Password": ctrl.loginWithPhoneAndPassword,
-        "Google Login": ctrl.loginWithGoogle,
-        "Apple Login": ctrl.loginWithApple,
-      };
-      
-      let login = null;
-      
-      for (const controller of Object.entries(loginMethods)) {
-        const result = controller({ email, phone, password, googleToken, appleToken });
-        if (result) {
-          login = true;
-          break; 
-        }
-      }
-      
-      if (login) {
-        res.status(200).json(login);
-      } else {
-        res.status(400).json({ message: "Invalid login credentials" });
-      }
+    ctrl.validateLoginData(email, phone, password, googleToken, appleToken, res)
 })
 
 module.exports = router;
