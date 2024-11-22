95f586c2408757bb2efc8980ccded912e71ce8d2
RoMan-robots <romkozhan@gmail.com>
2024-11-22 19:29:48 +0200

    added login route

---
+++ controllers/auth.js
@@ -0,0 +1,31 @@
+const loginWithEmailAndPassword = ({ email, password}) => {
+    if(email && password) {
+        // Логіка авторизації емейлу і пароля
+    } else {
+        return
+    }
+}
+
+const loginWithPhoneAndPassword = ({ phone, password}) => {
+    if(phone && password) {
+        // Логіка авторизації телефона і пароля
+    } else {
+        return
+    }
+}
+
+const loginWithGoogle = ({ googleToken }) => {
+    if(googleToken) {
+        // Логіка авторизації з гугл акаунта
+    } else {
+        return
+    }
+}
+
+const loginWithApple = ({ appleToken }) => {
+    if(appleToken) {
+        // Логіка авторизації з епл акаунта
+    } else {
+        return
+    }
+}
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,4 +3,30 @@ const ctrl = require('../../controllers/auth');
 
 const router = express.Router();
 
+router.post('/login', (req, res) => {
+    const { email, phone, password, googleToken, appleToken } = req.body;
+    const loginMethods = {
+        "Email and Password": ctrl.loginWithEmailAndPassword,
+        "Phone and Password": ctrl.loginWithPhoneAndPassword,
+        "Google Login": ctrl.loginWithGoogle,
+        "Apple Login": ctrl.loginWithApple,
+      };
+      
+      let login = null;
+      
+      for (const controller of Object.entries(loginMethods)) {
+        const result = controller({ email, phone, password, googleToken, appleToken });
+        if (result) {
+          login = true;
+          break; 
+        }
+      }
+      
+      if (login) {
+        res.status(200).json(login);
+      } else {
+        res.status(400).json({ message: "Invalid login credentials" });
+      }
+})
+
 module.exports = router;
