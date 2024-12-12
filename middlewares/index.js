db4c8b183329fe6e1663bf9cd5293f68b2332422
Mykola Kuts <nickkuts888@gmail.com>
2024-12-12 21:08:37 +0200

    added detectLangByHeaders

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,12 @@ const validateBody = require('./validateBody');
 const upload = require('./upload');
 const checkFileSize = require('./checkFileSize');
 const detectLanguageByIP = require('./detectLanguageByIP');
+const detectLangByHeader = require('./detectLangByHeader');
 
 module.exports = {
     validateBody,
     upload,
     checkFileSize,
     detectLanguageByIP,
+    detectLangByHeader,
 }
